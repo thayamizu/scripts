763aead8bb54fa66ffb7d0879be855558a624d54
速水　達也 <t.hayamizu@outlook.com>
2020-05-20 10:53:49 +0900

    Update password generation policy

--- sh/create-passwd.sh
+++ sh/create-passwd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- cat /dev/urandom | LC_CTYPE=C tr -dc '[:alnum:]' | head -c 12
+cat /dev/urandom | LC_CTYPE=C tr -dc '12345678abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ+\-!' | fold -w 12 | grep -E '[12345678]' | grep -E '[,\.+\-\!]' | head -5
